f476f180136dbc414d7aa54f45c3ae93a3144282
christianmilson <31931083+christianmilson@users.noreply.github.com>
2019-07-10 14:13:19 +0100

    Update route_model.h

--- src/route_model.h
+++ src/route_model.h
@@ -12,8 +12,6 @@ class RouteModel : public Model {
   public:
     class Node : public Model::Node {
       public:
-        // Add public Node variables and methods here.
-        
         Node(){}
         Node(int idx, RouteModel * search_model, Model::Node node) : Model::Node(node), parent_model(search_model), index(idx) {}
       
@@ -28,14 +26,12 @@ class RouteModel : public Model {
         bool visited = false;
         std::vector<Node*> neighbors;
       private:
-        // Add private Node variables and methods here.
         int index;
         RouteModel * parent_model = nullptr;
 
       	RouteModel::Node* FindNeighbor(std::vector<int> node_indices);
     };
     
-    // Add public RouteModel variables and methods here.
     RouteModel(const std::vector<std::byte> &xml);  
     std::vector<Node> path; // This variable will eventually store the path that is found by the A* search.
   
@@ -44,7 +40,6 @@ class RouteModel : public Model {
   	Node& FindClosestNode(float x, float y);
 
   private:
-    // Add private RouteModel variables and methods here.
     std::vector<Node> m_Nodes;
     std::unordered_map<int, std::vector<const Model::Road*>> node_to_road;
   
